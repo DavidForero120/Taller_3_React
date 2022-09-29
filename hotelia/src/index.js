11d08797950b11a298a98d6ce636f27078d838a0
Daro! <derodriguez074@misena.edu.co>
2022-09-28 23:06:14 -0500

    Dashboard

--- hotelia/src/index.js
+++ hotelia/src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Sidebar from './components/Sidebar/Sidebar'
+import Dashboard from './pages/Dashboard/Dashboard';
+
+import{
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+<BrowserRouter>
+  <Routes>
+    <Route path='/' element={<Dashboard />}></Route>
+  </Routes>
+</BrowserRouter>
+);
 reportWebVitals();
