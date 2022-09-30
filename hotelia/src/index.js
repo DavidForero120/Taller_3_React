c9099c938925850dbd201fa6cc681d4ed9f8c4f3
Daro! <derodriguez074@misena.edu.co>
2022-09-29 23:34:11 -0500

    Login en pages

--- hotelia/src/index.js
+++ hotelia/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Sidebar from './components/Sidebar/Sidebar'
 import Dashboard from './pages/Dashboard/Dashboard';
+import Login from './pages/login/login';
 
 import{
   BrowserRouter,
@@ -11,12 +12,17 @@ import{
   Route
 } from 'react-router-dom'
 
+const usuario ={
+  nombre : "Daniel",
+  correo :"daniel@gmail.com"
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
 <BrowserRouter>
   <Routes>
     <Route path='/' element={<Dashboard />}></Route>
+    <Route path='/usuario' element={<Login />}></Route>
   </Routes>
 </BrowserRouter>
 );
