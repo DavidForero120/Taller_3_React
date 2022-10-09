31397e06e92fb8dac1d2813c23d8c25ac9de3787
Daro! <derodriguez074@misena.edu.co>
2022-10-09 13:29:05 -0500

    Intengo de Registro :C

--- hotelia/src/components/formRegistro/form.jsx
+++ hotelia/src/components/formRegistro/form.jsx
@@ -1,10 +1,11 @@
 import axios from "axios";
 import { useState } from "react";
-import { Container, Form } from "react-bootstrap";
+import logo from './HoteliaHorizontalBlanco.svg';
+import './form.css';
 import { useNavigate } from "react-router";
 import Swal from 'sweetalert2'
 
-function LadoIzq(){
+function Form(){
 
     const history=useNavigate();
     
@@ -20,6 +21,7 @@ function LadoIzq(){
       paisorigen:"",
       password:"",
       tipouser:"",
+      foto:"",
       img:""});
     
     const handleChange=({target})=>{
@@ -54,141 +56,104 @@ function LadoIzq(){
         }
 
         return(
-            <div>
-        <Container>
-        <h1 className="text-center mt-3">Datos Huesped</h1>
-        <Form onSubmit={handleSubmit}>
-        <Form.Group className="mb-3">
-            <Form.Label>Tipo de Documento</Form.Label>
-            <Form.Select 
-            name="tipodoc"
-            onChange={handleChange}>
-                <option>Seleccione un Tipo de Documento</option>
-                <option value="Cédula de Ciudadanía">Cédula de Ciudadanía</option>
-                <option value="Tarjeta de Identidad">Tarjeta de Identidad</option>
-                <option value="Cédula de extranjería">Cédula de extranjería</option>
-            </Form.Select>
-            </Form.Group>
-
-            <Form.Group className="mb-3">
-                <Form.Label>No. Documento</Form.Label>
-                <Form.Control 
-                type="number" 
-                placeholder="Ingrese su número de documento"
-                name="_id" 
-                value={data._id}
-                onChange={handleChange}/> 
-            </Form.Group>
-
-            <Form.Group className="mb-3">
-                <Form.Label>Nombre</Form.Label>
-                <Form.Control 
-                type="text" 
-                placeholder="Ingrese su nombre"
-                name="nombre" 
-                value={data.nombre}
-                onChange={handleChange}/> 
-            </Form.Group>
-            
-            <Form.Group className="mb-3">
-                <Form.Label>Apellido</Form.Label>
-                <Form.Control 
-                type="text" 
-                placeholder="Ingrese su apellido"
-                name="apellido" 
-                value={data.apellido}
-                onChange={handleChange}/> 
-            </Form.Group>   
-
-            <Form.Group className="mb-3">
-                <Form.Label>Fecha de Nacimiento</Form.Label>
-                <Form.Control 
-                type="date" 
-                placeholder="Fehca"
-                name="fnacimiento" 
-                value={data.fnacimiento}
-                onChange={handleChange}/> 
-            </Form.Group>  
-
-             <Form.Group className="mb-3">
-                <Form.Label>Email</Form.Label>
-                <Form.Control 
-                type="text" 
-                placeholder="Email"
-                name="email" 
-                value={data.email}
-                onChange={handleChange}/> 
-            </Form.Group> 
-
-            <Form.Group className="mb-3">
-                <Form.Label>No. Telefono</Form.Label>
-                <Form.Control 
-                type="number" 
-                placeholder="Ingrese su número de telefono"
-                name="telefono" 
-                value={data.telefono}
-                onChange={handleChange}/> 
-            </Form.Group>
-
-             <Form.Group className="mb-3">
-            <Form.Label>Tipo de Documento</Form.Label>
-            <Form.Select 
-            name="paisorigen"
-            onChange={handleChange}>
-                <option>Seleccione el País de Origen</option>
-                <option value="Colombia">Colombia</option>
-                <option value="Venezuela">Venezuela</option>
-                <option value="Otro">Otro</option>
-            </Form.Select>
-            </Form.Group>           
-
-        
-            <Form.Group className="mb-3">
-                <Form.Label>Foto</Form.Label>
-                <Form.Control 
-                type="file" 
-                placeholder="Ingrese la URL de la imagen"
-                name="img" 
-                value={data.img}
-                onChange={handleChange}/> 
-            </Form.Group>
-
-            <Form.Group className="mb-3">
-                <Form.Label>Contraseña</Form.Label>
-                <Form.Control 
-                type="password" 
-                placeholder="Ingrese su contraseña"
-                name="password" 
-                value={data.password}
-                onChange={handleChange}/> 
-            </Form.Group>   
-
-            <Form.Group className="mb-3">
-                <Form.Label>Confirmar Contraseña</Form.Label>
-                <Form.Control 
-                type="password" 
-                placeholder="Ingrese su contraseña"
-                name="clave1" 
-                value={data.clave}
-                onChange={handleChange}/> 
-            </Form.Group>   
-            <Form.Group className="mb-3">
-                <Form.Label>Tipo Usuario</Form.Label>
-                <Form.Control 
-                type="text" 
-                placeholder="Ingrese su contraseña"
-                name="tipousuario" 
-                value={data.tipousuario}
-                onChange={handleChange}/> 
-            </Form.Group>   
-
-            
-            
-            <button className="btn btn-primary">Guardar</button>
-        </Form>
-        </Container>
-        </div>
+            <section id="container-grid">
+                <div class="logo">
+                    <div class="img">
+                    <img src={logo} alt="logo"/>
+                    <div class="bienvenido"><p>Eres más que bienvenido</p></div>
+                    <div class="text"><p>un lugar en tu corazón</p></div>
+                    </div>
+                </div>
+                    <div class="formulario1">
+                            <div class="tittle"><h1>Regístrate</h1></div>
+                            <div class="form1">
+                        <form onSubmit={handleSubmit}>
+                            <div class="doc">
+                                        <div id="inf">
+                                            <label for="tip-doc">Tipo de documento</label>
+                                            <select name="tipodoc" value={data.tipodoc} onChange={handleChange} id="tip-doc">
+                                                    <option value="0">Escoja una opción</option>
+                                                    <option value="Cedula de Ciudadania">Cédula</option>
+                                                    <option value="Tarjeta de Identidad">Tarjeta de identidad</option>
+                                                    <option value="Otro">Otro</option>
+                                            </select>
+                                        </div>
+                                        <div id="inf">
+                                            <label for="num-doc">Número documento</label>
+                                            <input type="text" name="_id" value={data._id} onChange={handleChange}  placeholder="10000000"/>
+                                        </div> 
+                            </div>
+                            <div class="nom1">
+                                <div id="inf">
+                                        <label for="nombres">Nombres</label>
+                                        <input type="text" name="nombre" value={data.nombre} onChange={handleChange}  placeholder="Nombres..."/>
+                                </div>
+                                <div id="inf">
+                                        <label for="Apellidos">Apellidos</label>
+                                        <input type="text" name="apellido" value={data.apellido} onChange={handleChange}  placeholder="Apellidos..."/>
+                                </div> 
+                            </div>
+                            <div class="dat">
+                                <div id="inf">
+                                        <label for="FecNac">Fecha de nacimiento</label>
+                                        <input type="date" name="fnacimiento" value={data.fnacimiento} onChange={handleChange} />
+                                </div>
+                                <div id="inf">
+                                        <div class="radio1">
+                                            <label for="genero">Género</label>
+                                            <input type="radio" name="genero" />Mujer
+                                            <input type="radio" name="genero"/>Hombre
+                                            <input type="radio" name="genero"/>Otro
+                                        </div>
+                                </div> 
+                            </div>
+                            <div class="corre">
+                                <div id="inf">
+                                        <label for="Email">Email</label>
+                                        <input type="email" name="email" value={data.email} onChange={handleChange} placeholder="correo..."/>
+                                </div>
+                                <div id="inf">
+                                        <label for="tel">Teléfono de contacto</label>
+                                        <input type="text" name="telefono" value={data.telefono} onChange={handleChange} placeholder="teléfono..."/>
+                                </div> 
+                            </div>
+                            <div class="orig">
+                                <div id="inf">
+                                        <label for="país">País deorigen</label>
+                                        <select name="paísorigen" value={data.paisorigen} onChange={handleChange} id="país">
+                                            <option value="0">Selecciona el Pais</option>
+                                            <option value="Colombia">Colombia</option>
+                                            <option value="Venezuela">Venezuela</option>
+                                            <option value="Narnia">Narnia</option>
+                                            <option value="Mozambique">Mozambique</option>
+                                        </select>
+                                </div>
+                                <div id="inf">
+                                        <label for="photo">Foto</label>
+                                        <input type="file" name="foto" value={data.foto} onChange={handleChange} placeholder="selecionar archivo"/>
+                                </div> 
+                            </div>
+                            <div class="contra1">
+                                        <div id="inf">
+                                            <label for="password">Contraseña</label>
+                                            <input type="password" name="password" value={data.password} onChange={handleChange} />
+                                        </div>
+                                        <div id="inf">
+                                            <label for="password2">Confirmar contraseña</label>
+                                            <input type="password2" name="password2"/>
+                                        </div> 
+                    
+                            </div>
+                            <div class="terminos">
+                                        <p><input type="checkbox"/> Términos y condiciones</p>
+                                        <button type="submit">Ingresar</button>
+                            </div>
+                        </form>
+                                    
+                        </div>  
+                    </div>
+                </section>
 
         );
 }
-export default LadoIzq;
+export default Form;
